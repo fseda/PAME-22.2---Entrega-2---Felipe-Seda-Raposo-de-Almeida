ffacd270ee495390bdcf7d180f9887ba17092a07
Felipe Seda <felipesedaraposo@gmail.com>
2023-01-11 21:00:50 -0300

    Remover comentários desnecessários

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// const fs = require("fs");
-
-// const name = document.getElementById('name');
-// const email = document.getElementById('email');
-
-// fs.readFile('submit.txt', (err, data) => {
-//   if (err) return console.error(err);
-
-//   if (data == 'true') {
-//     const success_message = document.getElementById('container-success');
-//     success_message.style.display = 'flex';
-//     console.log('helooo');
-//     setTimeout(() => {
-//       success_message.style.display = 'none';
-//       fs.writeFile('submit.txt', 'false', err => err ? console.error(err) : 0);
-//     }, 3000); 
-//   }
-// })
-
-// main_form.addEventListener('submit', () => {
-//   if (name && email) {
-//     fs.writeFile('submit.txt', 'true', err => err ? console.error(err) : 0);
-//   }
-// });
-
 function sendMessage() {
   const name = document.getElementById('name');
   const email = document.getElementById('email');
@@ -48,6 +23,3 @@ function sendMessage() {
   }
 }
 
-function required() {
-
-}
